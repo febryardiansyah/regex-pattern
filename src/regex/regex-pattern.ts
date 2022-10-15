1869d4a0b23fd4c2a91291c968203a8b06675d54
rairizky <rairizky37@gmail.com>
2022-10-15 16:25:42 +0700

    [ADD] base64 regex pattern

--- src/regex/regex-pattern.ts
+++ src/regex/regex-pattern.ts
@@ -144,12 +144,17 @@ export default class RegexPattern{
      * @OCExtension regex
      */
      static ocExtension = /.(xlsx|xls|csv)$/
-
-    /**
+     
+     /**
      * @pdfExtension regex
-     */
-    static pdfExtension = /.pdf$/
-    
+      */
+     static pdfExtension = /.pdf$/
+     
+     /**
+      * @Base64 regex
+      */
+      static base64 = /^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/
+
     /**
      * @binary regex
      */
